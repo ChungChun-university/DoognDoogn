99cf028a7619d02fe422608094739523703114dc
seongeun223 <jklun080921@gmail.com>
2024-08-05 06:41:04 +0900

    classEntity

--- Chungchun-university/src/main/java/com/chungchun/website/course/common/DTO.java
+++ Chungchun-university/src/main/java/com/chungchun/website/course/common/DTO.java
@@ -1,4 +1,22 @@
 package com.chungchun.website.course.common;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class DTO {
+
+    private int cl_no;
+    private String cl_name;
+    private LocalDate open_date;
+    private LocalDate close_date;
 }
