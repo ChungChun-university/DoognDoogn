b138f42ec02aae5a85b2421defd5ea243ae59e9a
seongeun223 <jklun080921@gmail.com>
2024-08-05 06:52:11 +0900

    classEntity

--- Chungchun-university/src/main/java/com/chungchun/website/course/common/Entity.java
+++ Chungchun-university/src/main/java/com/chungchun/website/course/common/Entity.java
@@ -1,4 +1,34 @@
 package com.chungchun.website.course.common;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@jakarta.persistence.Entity
+@Table(name="class")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Entity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cl_no")
+    private int cl_no;
+
+    @Column(name = "cl_name")
+    private String cl_name;
+
+    @Column(name = "open_date")
+    private LocalDate open_date;
+
+    @Column(name = "close_date")
+    private LocalDate close_date;
+
+
+
+
 }
