3e961cf076d595b9af678442e30b890607588880
지동현 <yjkl0516@gmail.com>
2024-08-05 18:29:40 +0900

    [Feat] - user.sql 더미 생성

--- Chungchun-university/src/main/resources/chungchundb/user.sql
+++ Chungchun-university/src/main/resources/chungchundb/user.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (user_id, user_pwd, user_name, user_phone, user_auth) VALUES
+INSERT INTO cc_user (user_id, user_pwd, user_name, user_phone, user_auth) VALUES
                                                                            ('ADMIN', '1234', '홍길동', '010-1234-5678', 'ROLE_ADMIN'),
                                                                            ('user2', '1234', '김앙츄', '010-2345-6789', 'ROLE_USER'),
                                                                            ('user3', '1234', '삼패기', '010-3456-7890', 'ROLE_USER'),
