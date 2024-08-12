44811286ad2c1fe92b0a076813d58278099e1bb8
seongeun223 <jklun080921@gmail.com>
2024-08-12 10:14:25 +0900

    [FEAT,FIX]
    ID 중복확인 수정 중
    좋아요 기능 구현
    관리자 로그인 후 관리자 페이지

---
+++ Chungchun-university/src/main/java/com/chungchun/website/webconfig/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,35 @@
+package com.chungchun.website.webconfig;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException, ServletException {
+        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException, IOException {
+        // 사용자 권한 확인
+        boolean isAdmin = authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+
+        if (isAdmin) {
+            response.sendRedirect("/admin/admin"); // 관리자 페이지
+        } else {
+            response.sendRedirect("/main"); // 일반 사용자 메인 페이지
+        }
+    }
+}
